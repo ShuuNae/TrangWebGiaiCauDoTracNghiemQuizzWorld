dc0c91c722673b22b853f7f1a1409f185695fd73
Nhung Van <nhungit.0498@gmail.com>
2020-12-15 14:40:24 +0700

    fontend

---
+++ src/app/pages/new-test/new-test.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NewTestComponent } from './new-test.component';
+
+describe('NewTestComponent', () => {
+  let component: NewTestComponent;
+  let fixture: ComponentFixture<NewTestComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NewTestComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NewTestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
